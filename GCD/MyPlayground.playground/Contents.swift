808d8e787e3ad70ad3f36b9d1e77769e24257f28
Pavel Chernyshev <titan100695@yandex.ru>
2020-09-28 21:51:57 +0300

    Groups

--- GCD/MyPlayground.playground/Contents.swift
+++ GCD/MyPlayground.playground/Contents.swift
@@ -25,14 +25,63 @@ class SafeArray<Element> {
     }
 }
 var safeArray = SafeArray<Int>()
-DispatchQueue.concurrentPerform(iterations: 10) { (index) in
+DispatchQueue.concurrentPerform(iterations: 5) { (index) in
     safeArray.append(element: index)
 }
 print("safeArray: \(safeArray.elements)")
 
 //without barrier
 var array = [Int]()
-DispatchQueue.concurrentPerform(iterations: 10) { (index) in
+DispatchQueue.concurrentPerform(iterations: 5) { (index) in
     array.append(index)
 }
 print("array: \(array)")
+//-----------------------------------------------------------
+// Groups
+let queue = DispatchQueue(label: "ru.swiftbook", attributes: .concurrent)
+let group = DispatchGroup()
+
+queue.async(group: group) {
+    for i in 0...10 {
+        if i == 10 {
+            print(i)
+        }
+    }
+}
+
+queue.async(group: group) {
+    for i in 0...20 {
+        if i == 20 {
+            print(i)
+        }
+    }
+}
+
+
+group.notify(queue: .main) {
+    print("All done in group: group")
+}
+
+let secondGroup = DispatchGroup()
+secondGroup.enter()
+queue.async(group: secondGroup) {
+    for i in 0...30 {
+        if i == 30 {
+            sleep(2)
+            print(i)
+            secondGroup.leave()
+        }
+    }
+}
+
+var result = secondGroup.wait(timeout: .now() + 1)
+print(result)
+
+secondGroup.notify(queue: .main) {
+    print("All done in group: secondGroup")
+}
+print("This print should be above")
+
+secondGroup.wait()
+
+print("Last print")
